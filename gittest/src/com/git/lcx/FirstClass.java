e7a22861ce0e9f59cc4bef344e470209b33c6830
louis <624382761@qq.com>
2019-09-15 16:46:57 +0800

    添加到git

---
+++ gittest/src/com/git/lcx/FirstClass.java
@@ -0,0 +1,8 @@
+package com.git.lcx;
+
+public class FirstClass {
+    public static void  main(String[] args){
+        System.out.print("22222222");
+        System.out.print("22222222");
+    }
+}
